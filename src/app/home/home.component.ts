901902b043d59b7ef0af45bc40a34cb9be710cec
wowshakhov <shakhov_vs@bw-sw.com>
2016-11-29 09:40:52 +0700

    home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../shared/auth.service';
+
 
 @Component({
   selector: 'my-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() {
-    // Do stuff
+  private title: string;
+  private loggedIn: boolean;
+
+  constructor (private auth: AuthService, private router: Router, private http: Http) {
+    this.title = this.auth.getName();
+    this.loggedIn = this.auth.isLoggedIn();
+
+    this.auth.loginObservable.subscribe(() => {
+      this.updateAccount();
+    });
+    this.auth.logoutObservable.subscribe(() => {
+      this.updateAccount();
+    });
   }
 
-  ngOnInit() {
-    console.log('Hello Home');
+  private updateAccount(): void {
+    this.title = this.auth.getName();
+    this.loggedIn = this.auth.isLoggedIn();
+  }
+
+  private logout(): void {
+    this.auth.logout()
+      .then(() => this.handleLogout())
+      .catch(error => alert(error));
+  }
+
+  private handleLogout(): void {
+    this.router.navigate(['/login']);
   }
 
 }
