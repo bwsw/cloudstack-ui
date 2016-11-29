fe7f7b19b357155f83f02b67125f84d38d9489c4
wowshakhov <shakhov_vs@bw-sw.com>
2016-11-29 09:34:06 +0700

    storage service

---
+++ src/app/shared/storage.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class StorageService {
+
+  constructor() {}
+
+  public write(key: any, value: any): void {
+    localStorage.setItem(key, value);
+  }
+
+  public read(key: any): any {
+    return localStorage.getItem(key);
+  }
+
+  public remove(key: any): void {
+    localStorage.removeItem(key);
+  }
+}
