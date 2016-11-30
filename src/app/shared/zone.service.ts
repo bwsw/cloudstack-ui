fc5fb4833f164b7976919e80684e74e5b4aeceff
Andrey Bents <bents_as@bw-sw.com>
2016-11-30 14:20:12 +0700

    #2738 added method to get zone by id

--- src/app/shared/zone.service.ts
+++ src/app/shared/zone.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
 import { AlertService, ApiRequestBuilderService } from '.';
 
 import 'rxjs/add/operator/toPromise';
@@ -17,6 +17,19 @@ export class ZoneService {
     private alert: AlertService
   ) { }
 
+  public get(id: string): Promise<Zone> {
+    let params: any = this.getDefaultParams();
+
+    params.id = id;
+
+    let request = this.requestBuilder.buildGETRequest(params);
+
+    return this.http.get(request)
+      .toPromise()
+      .then(response => response.json().listzonesresponse.zone[0] as Zone)
+      .catch(error => this.handleError(error));
+  }
+
   public getList(available = true): Promise<Array<Zone>> {
     let params: any = {
       command: 'listZones',
@@ -32,10 +45,25 @@ export class ZoneService {
     return this.http.get(request)
       .toPromise()
       .then(response => response.json().listzonesresponse.zone as Array<Zone>)
-      .catch(error => {
-        if (error.status === 401) {
-          this.alert.alert('You are not logged in');
-        }
-      });
+      .catch(error => this.handleError(error));
+  }
+
+  private getDefaultParams(): any {
+    return {
+      command: 'listZones',
+      response: 'json'
+    };
+  }
+
+  private handleError(error: Response): void {
+    switch (error.status) {
+      case 401:
+        this.alert.alert('You are not logged in');
+        break;
+
+      case 431:
+        this.alert.alert('Wrong arguments');
+        break;
+    }
   }
 }
