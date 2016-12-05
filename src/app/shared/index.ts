a90b858e69860ff3e33b8abcecaef0c32b3eb310
wowshakhov <shakhov_vs@bw-sw.com>
2016-12-05 13:11:52 +0700

    added services to index.ts

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -2,6 +2,7 @@ export * from './api.service';
 export * from './api-request-builder.service';
 export * from './alert.service';
 export * from './auth.service';
+export * from './config-reader.service';
 export * from './notification.service';
 export * from './storage.service';
 export * from './root-disk-size.service';
