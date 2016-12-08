e14db25c28ed85800e0e351b9f3149ce7a36c2f4
wowshakhov <shakhov_vs@bw-sw.com>
2016-12-08 10:37:36 +0700

    #2737 service offering service

--- src/app/shared/models/service-offering.model.ts
+++ src/app/shared/models/service-offering.model.ts
@@ -1,11 +1,6 @@
 import { BaseModel } from './base.model';
 import { FieldMapper } from '../decorators/field-mapper.decorator';
 
-// @FieldMapper({
-//   cpuNumber: 'cpunumber',
-//   cpuSpeed: 'cpuspeed',
-//   networkRate: 'networkrate'
-// })
 @FieldMapper({
   cpunumber: 'cpuNumber',
   cpuspeed: 'cpuSpeed',
