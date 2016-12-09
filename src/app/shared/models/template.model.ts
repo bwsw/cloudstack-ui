8c36443fcbd1f8a1c3d5e14e11664e77a23e8ce2
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-12-09 15:10:06 +0700

    #2736 Remap Fields in model

--- src/app/shared/models/template.model.ts
+++ src/app/shared/models/template.model.ts
@@ -1,14 +1,20 @@
 import { BaseModel } from './base.model';
+import { FieldMapper } from '../decorators';
 
 export interface ITag {
   key: string;
   value: string;
 }
 
-export class TemplateModel extends BaseModel {
+@FieldMapper({
+  displaytext: 'displayText',
+  ostypeid: 'osTypeId',
+  ostypename: 'osTypeName'
+})
+export class Template extends BaseModel {
   public name: string;
-  public displaytext: string;
-  public ostypeid: string;
-  public ostypename: string;
+  public displayText: string;
+  public osTypeId: string;
+  public osTypeName: string;
   public tags: Array<ITag>;
 }
