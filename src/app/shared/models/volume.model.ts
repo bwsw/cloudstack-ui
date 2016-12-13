020d8b40551adc80bc94bff4322f42e7889275a6
Andrey Bents <bents_as@bw-sw.com>
2016-12-13 13:21:41 +0700

    #2727 Os Type and volumes info added to virtual machine

--- src/app/shared/models/volume.model.ts
+++ src/app/shared/models/volume.model.ts
@@ -1,6 +1,11 @@
 import { BaseModel } from './base.model';
+import { FieldMapper } from '../decorators/field-mapper.decorator';
 
+@FieldMapper({
+  virtualmachineid: 'virtualMachineId'
+})
 export class Volume extends BaseModel {
   public name: string;
   public size: number;
+  public virtualMachineId: string;
 }
