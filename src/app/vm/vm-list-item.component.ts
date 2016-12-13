d973dfb9e0b3815f107bfaddfdb6335fa53b3fcb
Andrey Bents <bents_as@bw-sw.com>
2016-12-13 16:44:20 +0700

    #2727 list item rename fix

--- src/app/vm/vm-list-item.component.ts
+++ src/app/vm/vm-list-item.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 import { VirtualMachine } from './vm.model';
 
 @Component({
-  selector: 'cs-vm',
+  selector: 'cs-vm-list-item',
   templateUrl: './vm-list-item.component.html',
   styleUrls: ['./vm-list-item.component.scss']
 })
