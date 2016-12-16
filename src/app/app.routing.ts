7ec2a9692f46aca3c7b3bf7ba2e9c98d420d7bb1
Andrey Bents <bents_as@bw-sw.com>
2016-12-16 14:50:41 +0700

    #2735 instances page as a default root

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,30 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard, LoginGuard } from './shared/services';
-import { AboutComponent } from './about/about.component';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './auth/login.component';
 
 import { VmListComponent } from './vm/vm-list.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: HomeComponent,
-    canActivate: [
-      AuthGuard
-    ],
-    children: [
-      {
-        path: 'about',
-        component: AboutComponent,
-      },
-      {
-        path: 'instances',
-        component: VmListComponent,
-      }
-    ]
-  },
   {
     path: 'login',
     component: LoginComponent,
@@ -33,8 +14,15 @@ const routes: Routes = [
     ]
   },
   {
+    path: 'instances',
+    component: VmListComponent,
+    canActivate: [
+      AuthGuard
+    ]
+  },
+  {
     path: '**',
-    redirectTo: '',
+    redirectTo: '/instances',
   }
 ];
 
