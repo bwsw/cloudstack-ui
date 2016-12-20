86086b041c9fdbc9b96e2d88fc797f1aa73a4d8c
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-12-20 11:05:53 +0700

    added waiting containers

--- .build/build.sh
+++ .build/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Run node container to install dependencies, test and build app
-docker run --rm -v $(pwd):/workspace -w /workspace node npm i;
-docker run --rm -v $(pwd):/workspace -w /workspace node npm build;
+ID = docker run --rm -v $(pwd):/workspace -w /workspace node npm i;
+docker wait $ID
+ID = docker run --rm -v $(pwd):/workspace -w /workspace node npm build;
+docker wait $ID
 
-docker build -t cloudstack-nginx ./..;
+ID = docker build -t cloudstack-nginx ./..;
+docker wait $ID
 docker run -d -p 80:80 --name cloudstack-nginx cloudstack-nginx;
