1f326f211d20623132c20587ee2023c821ddfbf3
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-12-20 11:47:29 +0700

    test

--- .build/build.sh
+++ .build/build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 # Run node container to install dependencies, test and build app
-ID=docker run --rm -v $(pwd):/workspace -w /workspace node npm i;
+docker run --name node-install -v $(pwd):/workspace -w /workspace node npm i;
+ID=$(docker ps -aqf "name=node-install")
 docker wait $ID
-ID=docker run --rm -v $(pwd):/workspace -w /workspace node npm build;
-docker wait $ID
+docker rm $ID
 
-ID=docker build -t cloudstack-nginx ./..;
+docker run --name node-build -v $(pwd):/workspace -w /workspace node npm build;
+ID=$(docker ps -aqf "name=node-build")
 docker wait $ID
+docker rm $ID
+
+docker build -t cloudstack-nginx ./..;
 docker run -d -p 80:80 --name cloudstack-nginx cloudstack-nginx;
