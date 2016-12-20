eaeaa3f3188b4cabef522638f65f9a77f8e56069
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2016-12-20 16:17:50 +0700

    #2783 done

--- .build/build.sh
+++ .build/build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 # Run node container to install dependencies, test and build app
-docker run --name node-install -v $(pwd):/workspace -w /workspace node npm i;
-ID=$(docker ps -aqf "name=node-install")
-docker wait $ID
-docker rm $ID
-echo Dependencies was installed
+npm install;
+echo ******Dependencies was installed******
 
-docker run --name node-build -v $(pwd):/workspace -w /workspace node npm run build;
-ID=$(docker ps -aqf "name=node-build")
-docker wait $ID
-docker rm $ID
-echo Dist was builded
+npm run build
+echo ******Dist was builded******
 
+# Build docker image
 docker build -t cloudstack-nginx --file $(pwd)/Dockerfile $(pwd);
 
+# Check if nginx is running and then stop it
 NGINXID=$(docker ps -aqf "name=cloudstack-nginx")
 if [[ -n $NGINXID ]]; then
   docker stop $NGINXID;
+  docker rm $NGINXID;
 fi
 
-echo Starting Nginx
+# Starting server
+echo ******Starting Nginx******
 docker run -d -p 80:80 --name cloudstack-nginx cloudstack-nginx;
+
+
 
 
--- .build/nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run -p 80:80 --name docker-nginx --rm -v $(dirname $(pwd))/dist:/var/www/dist -v $(pwd)/nginx.conf:/etc/nginx/conf.d/default.conf nginx
-
