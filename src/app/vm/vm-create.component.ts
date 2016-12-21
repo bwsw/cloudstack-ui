85c3b7a2dad2418eb2ece38a923d3a6295d2f38e
wowshakhov <shakhov_vs@bw-sw.com>
2016-12-21 17:44:16 +0700

    #2721 vm creation page

--- src/app/vm/vm-create.component.ts
+++ src/app/vm/vm-create.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { VmService } from './vm.service';
 import { TranslateService } from 'ng2-translate';
-
+import { ZoneService } from '../shared/services/zone.service';
+import { Zone } from '../shared/models/zone.model';
 
 @Component({
   selector: 'cs-vm-create',
@@ -11,12 +12,24 @@ import { TranslateService } from 'ng2-translate';
 })
 export class VmCreateComponent implements OnInit {
 
+  public rootDiskSize: number;
+  public zones: Array<Zone>;
+  public zone: number;
+
   constructor (
     private vmService: VmService,
     private translateService: TranslateService,
-  ) { }
+    private zoneService: ZoneService
+  ) {
+    this.rootDiskSize = 0;
+    this.zones = [];
+    this.zone = 0;
+  }
 
   public ngOnInit() {
-
+    this.zoneService.getList().then(result => {
+      this.zones = result;
+      this.zone = result[0].id;
+    })
   }
 }
