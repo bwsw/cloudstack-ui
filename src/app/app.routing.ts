a21fec39c3bee616fb0c7c8ea3d39fed6cda3624
Andrey Bents <bents_as@bw-sw.com>
2016-12-28 17:32:59 +0700

    #2730 debug info removed

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -4,8 +4,6 @@ import { AuthGuard, LoginGuard } from './shared/services';
 import { LoginComponent } from './auth/login.component';
 
 import { VmListComponent } from './vm/vm-list.component';
-import { SecurityGroupCreationComponent } from './security-group/security-group-creation.component';
-import { asd } from './security-group/asd.component';
 
 const routes: Routes = [
   {
@@ -18,14 +16,6 @@ const routes: Routes = [
   {
     path: 'instances',
     component: VmListComponent,
-    canActivate: [
-      AuthGuard
-    ]
-  },
-  {
-    path: 'sg',
-    component: SecurityGroupCreationComponent,
-    // component: asd,
     canActivate: [
       AuthGuard
     ]
