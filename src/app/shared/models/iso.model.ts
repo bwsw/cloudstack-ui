68d9de0c67c5577355e5c3e77d74c37c64b18255
bents_as <bents_as@bw-sw.com>
2017-02-06 10:46:12 +0700

    #3073 added fields to template and iso models

--- src/app/shared/models/iso.model.ts
+++ src/app/shared/models/iso.model.ts
@@ -4,10 +4,24 @@ import { FieldMapper } from '../decorators';
 
 @FieldMapper({
   displaytext: 'displayText',
+  isextractable: 'isExtractable',
+  isfeatured: 'isFeatured',
+  ispublic: 'isPublic',
+  isready: 'isReady',
   ostypeid: 'osTypeId',
   ostypename: 'osTypeName'
 })
 export class Iso extends BaseModel {
   public id: string;
+  public bootable: boolean;
+  public displayText: string;
+  public isExtractable: boolean;
+  public isFeatured: boolean;
+  public isPublic: boolean;
+  public isReady: boolean;
+  public osTypeId: string;
+  public osTypeName: string;
+  public name: string;
+  public status: string;
   public size: number;
 }
