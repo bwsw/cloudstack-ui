b2626e1356818709218883b1ddc5eee627e6b000
bents_as <bents_as@bw-sw.com>
2017-02-07 11:51:36 +0700

    #3080 os family map added

--- src/app/shared/services/os-type.service.ts
+++ src/app/shared/services/os-type.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 import { BaseBackendService } from '.';
 import { BackendResource } from '../decorators';
-import { OsType } from '../models/os-type.model';
+import { OsType, OsFamily } from '../models/os-type.model';
 
 
 @Injectable()
@@ -10,4 +11,39 @@ import { OsType } from '../models/os-type.model';
   entity: 'OsType',
   entityModel: OsType
 })
-export class OsTypeService extends BaseBackendService<OsType> { }
+export class OsTypeService extends BaseBackendService<OsType> {
+  public getList(params?: {}): Observable<Array<OsType>> {
+    return super.getList(params)
+      .map(osTypes => {
+        osTypes.forEach(osType => {
+          osType.osFamily = this.mapOsFamily(osType.description);
+        });
+
+        return osTypes;
+      });
+  }
+
+  private mapOsFamily(osName): OsFamily {
+    const windows: OsFamily = 'Windows';
+    if (osName.includes(windows) || osName.includes('Microsoft')) {
+      return windows;
+    }
+
+    const macOs: OsFamily = 'Mac OS';
+    if (osName.includes(macOs)) {
+      return macOs;
+    }
+
+    const linux: OsFamily = 'Linux';
+    if (osName.includes(linux)) {
+      return linux;
+    }
+
+    if (osName.includes('CentOS') || osName.includes('Debian') ||
+      osName.includes('Fedora') || osName.includes('Ubuntu')) {
+      return linux;
+    }
+
+    return 'Other';
+  }
+}
