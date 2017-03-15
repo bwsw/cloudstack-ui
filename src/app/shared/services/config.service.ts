ef5661b1ef104fe946e6e808057d4c9c8283b779
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2017-03-15 09:44:47 +0700

    #3379 added multiple loading to config service

--- src/app/shared/services/config.service.ts
+++ src/app/shared/services/config.service.ts
@@ -13,28 +13,30 @@ export class ConfigService {
 
   constructor(private http: Http) { }
 
-  public get(key: string | Array<string>): Observable<any> {
-    if (Array.isArray(key)) {
-      // if (this.config) {
-      //   let result = [];
-      //
-      //   for (let k in this.config) {
-      //     if ((this.config as Object).hasOwnProperty(k)) {
-      //       result.push(this.config[k]);
-      //     }
-      //   }
-      //
-      //   return Observable.of(result);
-      // } else {
-      //   return this.load().map(() => this.config[key]);
-      // }
-    } else {
-      if (this.config) {
-        return Observable.of(this.config[key]);
-      } else {
-        return this.load().map(() => this.config[key]);
+  public get(key: string | Array<string>): Observable<any | Array<any>> {
+    let isArray = Array.isArray(key);
+
+    if (this.config) {
+      return Observable.of(this.getResult(isArray, key));
+    }
+
+    return this.load().map(() => this.getResult(isArray, key));
+  }
+
+  private getResult(isArray: boolean, key: string | Array<string>): any {
+    return isArray ? this.getArrayResult(key as Array<string>) : this.config[key as string];
+  }
+
+  private getArrayResult(keyArray: Array<string>): Array<any> {
+    let result = [];
+
+    for (let key in this.config) {
+      if ((this.config as Object).hasOwnProperty(key) && keyArray.includes(key)) {
+        result.push(this.config[key]);
       }
     }
+
+    return result;
   }
 
   private load(reload = false): Observable<any> {
