32a62120053b6a9041689d7de0572f5b5c6d1d5a
bents_as <bents_as@bw-sw.com>
2017-03-24 15:25:18 +0700

    #3456 add ssh key creation

--- src/app/ssh-keys/ssh-keys-page.component.ts
+++ src/app/ssh-keys/ssh-keys-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MdlDialogService } from 'angular2-mdl';
+import { TranslateService } from 'ng2-translate';
 
 import { SSHKeyPair } from '../shared/models/ssh-keypair.model';
-import { SSHKeyPairService } from '../shared/services/SSHKeyPair.service';
+import { SSHKeyPairService } from '../shared/services/ssh-keypair.service';
+import { SShKeyCreationDialogComponent } from './ssh-key-creation-dialog.component';
 
 
 @Component({
@@ -11,7 +14,11 @@ import { SSHKeyPairService } from '../shared/services/SSHKeyPair.service';
 export class SshKeysPageComponent implements OnInit {
   public sshKeyList: Array<SSHKeyPair>;
 
-  constructor(private sshKeyService: SSHKeyPairService) { }
+  constructor(
+    private sshKeyService: SSHKeyPairService,
+    private dialogService: MdlDialogService,
+    private translateService: TranslateService
+  ) { }
 
   public ngOnInit(): void {
     this.sshKeyService
@@ -19,4 +26,63 @@ export class SshKeysPageComponent implements OnInit {
       .subscribe(keyList => this.sshKeyList = keyList);
   }
 
+  public showCreationDialog(): void {
+    this.dialogService.showCustomDialog({
+      component: SShKeyCreationDialogComponent,
+      styles: { width: '400px' }
+    })
+      .switchMap(res => res.onHide())
+      .subscribe(data => this.createSshKey(data));
+  }
+
+  private createSshKey(data) {
+    if (!data) {
+      return;
+    }
+
+    const keyCreationObs = data.publicKey ? this.sshKeyService.register(data) : this.sshKeyService.create(data);
+    keyCreationObs.subscribe(
+      (sshKey: SSHKeyPair) => {
+        this.sshKeyList = this.sshKeyList.concat(sshKey);
+        if (sshKey.privateKey) {
+          this.showPrivateKey(sshKey.privateKey);
+        }
+      },
+      (error) => this.handleError(error)
+    );
+  }
+
+  private showPrivateKey(privateKey: string): void {
+    this.dialogService.showDialog({
+      message: privateKey,
+      actions: [{
+        handler: () => { },
+        text: 'Ok' ,
+        isClosingAction: true
+      }],
+      isModal: true,
+      styles: { width: '400px', 'word-break': 'break-all' }
+    });
+  }
+
+  private handleError(error): void {
+    const errorMap = {
+      'A key pair with name': 'KEYPAIR_ALREADY_EXISTS',
+      'Public key is invalid': 'INVALID_PUBLIC_KEY'
+    };
+
+    let message;
+    for (let key in errorMap) {
+      if (error.errortext.startsWith(key)) {
+        message = errorMap[key];
+        break;
+      }
+    }
+    if (!message) {
+      message = error.errortext;
+    }
+
+    this.translateService.get(message)
+      .subscribe((message) => this.dialogService.alert(message));
+  }
 }
--- src/app/ssh-keys/ssh-keys.module.ts
+++ src/app/ssh-keys/ssh-keys.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { MdlModule } from 'angular2-mdl';
+import { TranslateModule } from 'ng2-translate';
 
 import { SshKeyListComponent } from './ssh-key-list.component';
 import { SharedModule } from '../shared/shared.module';
 import { SshKeysPageComponent } from './ssh-keys-page.component';
+import { SShKeyCreationDialogComponent } from './ssh-key-creation-dialog.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     SharedModule,
+    TranslateModule,
     MdlModule
   ],
   exports: [SshKeysPageComponent],
   declarations: [
     SshKeyListComponent,
-    SshKeysPageComponent
+    SshKeysPageComponent,
+    SShKeyCreationDialogComponent
   ],
+  entryComponents: [SShKeyCreationDialogComponent]
 })
 export class SshKeysModule { }
