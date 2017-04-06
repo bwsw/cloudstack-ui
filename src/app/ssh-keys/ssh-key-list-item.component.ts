6d89dcce5c702642cf916869fee38fc2df78df0d
bents_as <bents_as@bw-sw.com>
2017-04-06 12:37:22 +0700

    #3532 add key icon to ssh key header

--- src/app/ssh-keys/ssh-key-list-item.component.ts
+++ src/app/ssh-keys/ssh-key-list-item.component.ts
@@ -5,10 +5,7 @@ import { SSHKeyPair } from '../shared/models/ssh-keypair.model';
 @Component({
   selector: 'cs-ssh-key-list-item',
   templateUrl: 'ssh-key-list-item.component.html',
-  styles: [`
-    :host { display: inline-flex }
-    :host mdl-card { width: 340px }
-  `]
+  styleUrls: ['ssh-key-list-item.component.scss']
 })
 export class SshKeyListItemComponent {
   @Input() public key: SSHKeyPair;
