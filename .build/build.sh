a317b23bc422c05ae0fbc10728fb1ab480f9c635
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2017-04-11 12:30:00 +0700

    #3180 added configurable config_path to run.sh, added bundle gzipping

--- .build/build.sh
+++ .build/build.sh
@@ -9,27 +9,3 @@ echo ******Dist was built******
 
 # Build docker image
 docker build -t cloudstack-nginx --file=Dockerfile ..
-
-# Check if DEPLOY_PORT wasn't set
-if [ -z "$DEPLOY_PORT" ]; then
-  export DEPLOY_PORT="80";
-fi
-
-# Generate container name unique for port
-CONTAINER_NAME=cloudstack-nginx
-CONTAINER_NAME=$CONTAINER_NAME-$DEPLOY_PORT
-
-# Check if nginx is running and then stop it
-NGINX_ID=$(docker ps -aqf "name=$CONTAINER_NAME")
-if [[ -n $NGINX_ID ]]; then
-  docker stop $NGINX_ID;
-  docker rm $NGINX_ID;
-fi
-
-# Starting server
-echo ******Starting Nginx******
-docker run -e "API_BACKEND_URL=$API_BACKEND_URL" -d -p $DEPLOY_PORT:80 --name $CONTAINER_NAME cloudstack-nginx;
-
-
-
-
---
+++ .build/run.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+# Check if DEPLOY_PORT wasn't set
+if [ -z "$DEPLOY_PORT" ]; then
+  export DEPLOY_PORT="80";
+fi
+
+if [ -n "$CONFIG_PATH" ]; then
+  export CONFIG_MOUNT="-v ${CONFIG_PATH}:/var/www/dist/config";
+else
+  export CONFIG_MOUNT="";
+fi
+
+# Generate container name unique for port
+CONTAINER_NAME=cloudstack-nginx
+CONTAINER_NAME=$CONTAINER_NAME-$DEPLOY_PORT
+
+# Check if nginx is running and then stop it
+NGINX_ID=$(docker ps -aqf "name=$CONTAINER_NAME")
+if [[ -n $NGINX_ID ]]; then
+  docker stop $NGINX_ID;
+  docker rm $NGINX_ID;
+fi
+
+# Starting server
+echo ******Starting Nginx******
+echo "CONFIG_PATH=${CONFIG_MOUNT}";
+docker run -e "API_BACKEND_URL=$API_BACKEND_URL" -d -p $DEPLOY_PORT:80 --name $CONTAINER_NAME $CONFIG_MOUNT cloudstack-nginx;
