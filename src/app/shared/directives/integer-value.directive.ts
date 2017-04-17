94169833e88328142a050ba963ff800fac31636b
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2017-04-17 13:07:15 +0700

    #3653 add additional tcp/udp port validation

---
+++ src/app/shared/directives/integer-value.directive.ts
@@ -0,0 +1,23 @@
+import { Validators, Validator, NG_VALIDATORS, FormControl } from '@angular/forms';
+import { Directive, OnInit } from '@angular/core';
+import { integerValidator } from './integer-validator';
+
+
+@Directive({
+  // tslint:disable-next-line
+  selector: '[integerValue]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: IntegerValidatorDirective, multi: true }
+  ]
+})
+export class IntegerValidatorDirective implements OnInit, Validator {
+  private validator = Validators.nullValidator;
+
+  public ngOnInit(): void {
+    this.validator = integerValidator();
+  }
+
+  public validate(c: FormControl): { [key: string]: any; } {
+    return this.validator(c);
+  }
+}
