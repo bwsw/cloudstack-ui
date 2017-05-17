9cd2cbfe10017723fcba823251e6fdf1d3749558
andrewbents <bents_as@bw-sw.com>
2017-05-17 10:13:04 +0700

    add disk offering info to spare drive sidebar

--- src/app/spare-drive/spare-drive-sidebar/spare-drive-sidebar.component.ts
+++ src/app/spare-drive/spare-drive-sidebar/spare-drive-sidebar.component.ts
@@ -1,6 +1,9 @@
-import { Component, Input, HostBinding } from '@angular/core';
-import { Volume } from '../../shared/models';
+import { Component, HostBinding, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { Volume } from '../../shared/models';
+import { VolumeTypes } from '../../shared/models/volume.model';
+import { DiskOfferingService } from '../../shared/services/disk-offering.service';
 import { VolumeService } from '../../shared/services/volume.service';
 
 
@@ -14,14 +17,22 @@ export class SpareDriveSidebarComponent {
 
   constructor(
     private route: ActivatedRoute,
-    private volumeService: VolumeService
+    private volumeService: VolumeService,
+    private diskOfferingService: DiskOfferingService
   ) {
     this.route.params.pluck('id')
       .subscribe((id: string) => {
         if (id) {
-        this.volumeService.get(id)
-          .subscribe(vm => this.volume = vm);
-      }
+          Observable.forkJoin(
+            this.diskOfferingService.getList({ type: VolumeTypes.DATADISK }),
+            this.volumeService.get(id)
+          ).subscribe(([diskOfferings, volume]) => {
+            volume.diskOffering = diskOfferings.find(
+              offering => offering.id === volume.diskOfferingId
+            );
+            this.volume = volume;
+          });
+        }
     });
   }
 }
