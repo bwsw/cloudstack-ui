3abde79317e1c137784847b86d8315a284392032
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2017-07-05 14:37:38 +0700

    #28 add security group templates to mock entity data

--- src/testutils/mocks/model-services/entity-data.ts
+++ src/testutils/mocks/model-services/entity-data.ts
@@ -1,3 +1,4 @@
+import { SecurityGroup } from '../../../app/security-group/sg.model';
 import { AffinityGroup, DiskOffering, ServiceOffering, SSHKeyPair, Zone } from '../../../app/shared/models';
 import { Iso, Template } from '../../../app/template/shared';
 
@@ -5,6 +6,7 @@ import { Iso, Template } from '../../../app/template/shared';
 const affinityGroups: Array<Object> = require('../fixtures/affinityGroups.json');
 const diskOfferings: Array<Object> = require('../fixtures/diskOfferings.json');
 const isos: Array<Object> = require('../fixtures/isos.json');
+const securityGroupTemplates: Array<Object> = require('../fixtures/securityGroupTemplates.json');
 const serviceOfferings: Array<Object> = require('../fixtures/serviceOfferings.json');
 const sshKeyPairs: Array<Object> = require('../fixtures/sshKeyPairs.json');
 const templates: Array<Object> = require('../fixtures/templates.json');
@@ -14,6 +16,7 @@ export class MockEntityData {
   public affinityGroups: Array<AffinityGroup>;
   public diskOfferings: Array<DiskOffering>;
   public isos: Array<Iso>;
+  public securityGroupTemplates: Array<SecurityGroup>;
   public serviceOfferings: Array<ServiceOffering>;
   public sshKeyPairs: Array<SSHKeyPair>;
   public templates: Array<Template>;
@@ -23,6 +26,7 @@ export class MockEntityData {
     this.affinityGroups = affinityGroups.map(json => new AffinityGroup(json));
     this.diskOfferings = diskOfferings.map(json => new DiskOffering(json));
     this.isos = isos.map(json => new Iso(json));
+    this.securityGroupTemplates = securityGroupTemplates.map(json => new SecurityGroup(json));
     this.serviceOfferings = serviceOfferings.map(json => new ServiceOffering(json));
     this.sshKeyPairs = sshKeyPairs.map(json => new SSHKeyPair(json));
     this.templates = templates.map(json => new Template(json));
