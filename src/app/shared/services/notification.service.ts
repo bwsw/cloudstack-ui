6e0bea0b031409ead0050ab030e2244fd06eabd7
Darya Baklanova <darya.baklanova@gmail.com>
2017-07-28 10:13:35 +0700

    feat(notification): replace mdl snack-bar by md snack-bar
    replace mdl snack-bar service by md snack-bar component

--- src/app/shared/services/notification.service.ts
+++ src/app/shared/services/notification.service.ts
@@ -2,45 +2,68 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { TranslateService } from '@ngx-translate/core';
 import { ParametrizedTranslation } from '../../dialog/dialog-module/dialog.service';
-import { MdSnackBar } from '@angular/material';
+import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 
 @Injectable()
 export class NotificationService implements NotificationService {
-  public timeout: number;
+  public snackBarConfig: MdSnackBarConfig;
 
   constructor(
     private snackBar: MdSnackBar,
     private translateService: TranslateService
   ) {
-    this.timeout = 2750;
+    this.snackBarConfig = { duration: 3000 };
   }
 
-  public message(message: string | ParametrizedTranslation): any {
+  public message(
+    message: string | ParametrizedTranslation,
+    config?: MdSnackBarConfig
+  ): any {
+    if (config) {
+      this.snackBarConfig = config;
+    }
+
     return this.getTranslatedMessage(message)
-      .subscribe(translatedMessage => this.snackBar.open(translatedMessage));
+      .subscribe(translatedMessage => this.snackBar
+        .open(translatedMessage, null, this.snackBarConfig));
   }
 
   public warning(
     message: string | ParametrizedTranslation,
-    action?:  string
+    action: string,
+    config?: MdSnackBarConfig
   ) {
-   return this.getTranslatedMessage(message)
-     .subscribe( translatedMessage => this.snackBar.open(translatedMessage, action));
+    if (config) {
+      this.snackBarConfig = config;
+    }
+
+    return this.getTranslatedMessage(message)
+      .subscribe(translatedMessage => this.snackBar
+        .open(translatedMessage, action, this.snackBarConfig));
   }
 
   public error(
     message: string | ParametrizedTranslation,
-    action?: string
-    ) {
+    action?: string,
+    config?: MdSnackBarConfig
+  ) {
+    if (config) {
+      this.snackBarConfig = config;
+    }
+
     return this.getTranslatedMessage(message)
-      .subscribe( translatedMessage => this.snackBar.open(translatedMessage, action));
+      .subscribe(translatedMessage => this.snackBar
+        .open(translatedMessage, action, this.snackBarConfig));
   }
 
   private getTranslatedMessage(message: string | ParametrizedTranslation): Observable<string> {
     if (typeof message === 'string') {
       return this.translateService.get(message);
     } else {
-      return this.translateService.get(message.translationToken, message.interpolateParams);
+      return this.translateService.get(
+        message.translationToken,
+        message.interpolateParams
+      );
     }
   }
 }
