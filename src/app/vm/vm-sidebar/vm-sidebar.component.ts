29418ef3eb2378e40cb8a20a89dbee9e110a92d3
wowshakhov <shakhov_vs@bw-sw.com>
2017-08-10 15:35:19 +0700

    #367 show 'vm not found' message if vmId in query params is invalid

--- src/app/vm/vm-sidebar/vm-sidebar.component.ts
+++ src/app/vm/vm-sidebar/vm-sidebar.component.ts
@@ -1,9 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { NotificationService } from '../../shared/services/notification.service';
 import { VirtualMachine } from '../shared/vm.model';
 import { VmService } from '../shared/vm.service';
-import { NotificationService } from '../../shared/services/notification.service';
 
 
 @Component({
@@ -13,18 +12,25 @@ import { NotificationService } from '../../shared/services/notification.service'
 })
 export class VmSidebarComponent {
   @Input() public vm: VirtualMachine;
+  public vmNotFound: boolean;
 
   constructor(
     private vmService: VmService,
     private notificationService: NotificationService,
     private route: ActivatedRoute
   ) {
+    this.vmNotFound = false;
     this.route.params.pluck('id')
       .filter(id => !!id)
       .switchMap((id: string) => this.vmService.getWithDetails(id))
       .subscribe(
-        vm => this.vm = vm,
-        (error) => this.notificationService.error(error.message)
-      );
+        vm => {
+          if (vm) {
+            this.vm = vm;
+          } else {
+            this.vmNotFound = true;
+          }
+        },
+        (error) => this.notificationService.error(error.message));
   }
 }
