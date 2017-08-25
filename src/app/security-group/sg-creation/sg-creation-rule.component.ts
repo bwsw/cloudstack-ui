851bdf0e7b5fddb79158df1d491a385e76baf615
Darya Baklanova <darya.baklanova@gmail.com>
2017-08-25 15:23:55 +0700

    feat(sg-rules): change the view of security group rules
    change the view of security group rules

--- src/app/security-group/sg-creation/sg-creation-rule.component.ts
+++ src/app/security-group/sg-creation/sg-creation-rule.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { NetworkProtocol } from '../sg.model';
+import {
+  GetICMPCodeTranslationToken, GetICMPTypeTranslationToken,
+  NetworkProtocol
+} from '../sg.model';
 import { RuleListItem } from './sg-creation.component';
 
 
@@ -12,4 +15,31 @@ export class SgCreationRuleComponent {
   @Input() public type: string;
 
   public NetworkProtocols = NetworkProtocol;
+
+  public get protocolTranslationToken(): string {
+    const protocolTranslations = {
+      'TCP': 'SECURITY_GROUP_PAGE.RULES.TCP',
+      'UDP': 'SECURITY_GROUP_PAGE.RULES.UDP',
+      'ICMP': 'SECURITY_GROUP_PAGE.RULES.ICMP'
+    };
+
+    return protocolTranslations[this.item.rule.protocol.toUpperCase()];
+  }
+
+  public get typeTranslationToken(): string {
+    const typeTranslations = {
+      'INGRESS': 'SECURITY_GROUP_PAGE.RULES.INGRESS_DISPLAY',
+      'EGRESS': 'SECURITY_GROUP_PAGE.RULES.EGRESS_DISPLAY'
+    };
+
+    return typeTranslations[this.type.toUpperCase()];
+  }
+
+  public get icmpTypeTranslationToken(): string {
+    return GetICMPTypeTranslationToken(this.item.rule.icmpType);
+  }
+
+  public get icmpCodeTranslationToken(): string {
+    return GetICMPCodeTranslationToken(this.item.rule.icmpType, this.item.rule.icmpCode);
+  }
 }
