be912afd4affbff6200cfb34a2dfdf146e71b228
Ivaschenko Christina <caloriees@gmail.com>
2017-11-23 14:19:50 +0700

    feat(ngrx-auth): Clear NGRX Store if user logs out

--- src/app/reducers/auth/redux/auth.actions.ts
+++ src/app/reducers/auth/redux/auth.actions.ts
@@ -2,6 +2,8 @@ import { Action } from '@ngrx/store';
 
 export const LOAD_USER_ACCOUNT_REQUEST = '[USER ACCOUNT] LOAD_USER_ACCOUNT_REQUEST';
 export const LOAD_USER_ACCOUNT_RESPONSE = '[USER ACCOUNT] LOAD_USER_ACCOUNT_RESPONSE';
+export const LOG_OUT_USER_ACCOUNT = '[USER ACCOUNT] LOG_OUT_USER_ACCOUNT';
+
 
 export class LoadUserAccountRequest implements Action {
   type = LOAD_USER_ACCOUNT_REQUEST;
@@ -19,5 +21,14 @@ export class LoadUserAccountResponse implements Action {
 
 }
 
+export class LogOutUserAccount implements Action {
+  type = LOG_OUT_USER_ACCOUNT;
+
+  constructor(public payload?: any) {
+  }
+
+}
+
 export type Actions = LoadUserAccountRequest
-  | LoadUserAccountResponse;
+  | LoadUserAccountResponse
+  | LogOutUserAccount;
