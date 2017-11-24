4bf512888e88067ed3cbb84e4bba3bfab44359b7
Darya Baklanova <darya.baklanova@gmail.com>
2017-11-24 14:45:24 +0700

    feat(service-offering) add possibility to set custom service offering

---
+++ proxy-conf.js
@@ -0,0 +1,25 @@
+const PROXY_CONFIG = [
+  {
+    context: [
+      "/client/api",
+      "/client/console"
+    ],
+    target: "https://cs2-1.netpoint-dc.com/",
+    // target: "http://cs-simulator-dev.z1.netpoint-dc.com:8080/",
+    secure: false
+  },
+  {
+    context: ["/csextensions/pulse/**"],
+    target: "http://192.168.1.218:8082/",
+    secure: false,
+    pathRewrite: {'^/csextensions/pulse': ''}
+  },
+  {
+    context: ["/csextensions/webshell/**"],
+    target: "http://192.168.1.218:8018/",
+    secure: false,
+    pathRewrite: {'^/csextensions/webshell': ''}
+  }
+];
+
+module.exports = PROXY_CONFIG;
