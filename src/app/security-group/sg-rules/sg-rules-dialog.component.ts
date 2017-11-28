b2d0918b4a90e9218b14a650deabfdef9946d71f
Darya Baklanova <darya.baklanova@gmail.com>
2017-11-28 10:42:43 +0700

    fix(security groups) redirect for editing sg rules to sg page

---
+++ src/app/security-group/sg-rules/sg-rules-dialog.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+import { SgRulesContainerComponent } from '../containers/sg-rules.container';
+
+@Component({
+  selector: 'cs-sg-rules-dialog',
+  template: ``
+})
+export class SecurityGroupRulesDialogComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private router: Router
+  ) {
+    const params = this.route.snapshot.params;
+    this.showDialog(params['id']);
+  }
+
+  private showDialog(securityGroupId: string) {
+    const editMode = !!this.route.snapshot.queryParams.hasOwnProperty('vm');
+
+    this.dialog.open(SgRulesContainerComponent, <MatDialogConfig>{
+      width: '910px',
+      data: { securityGroupId, editMode }
+    });
+
+    this.dialog.afterAllClosed.subscribe(() => {
+      this.router.navigate(
+        ['../../'],
+        { queryParamsHandling: 'preserve', relativeTo: this.route }
+      );
+    });
+  }
+}
