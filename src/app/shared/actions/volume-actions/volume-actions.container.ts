fcc59cba7dfe4f41126082d7c8cb0ef2fd4f309f
ksendart <ksendart@yandex.ru>
2017-11-29 09:36:22 +0700

    #630-vm-ngrx - attachment bug

--- src/app/shared/actions/volume-actions/volume-actions.container.ts
+++ src/app/shared/actions/volume-actions/volume-actions.container.ts
@@ -1,4 +1,6 @@
 import {
+  AfterViewInit,
+  ChangeDetectorRef,
   Component,
   Input
 } from '@angular/core';
@@ -7,7 +9,6 @@ import { Store } from '@ngrx/store';
 import { State } from '../../../reducers/index';
 import { Volume } from '../../models/volume.model';
 import * as volumeActions from '../../../reducers/volumes/redux/volumes.actions';
-import { WithUnsubscribe } from '../../../utils/mixins/with-unsubscribe';
 import { AuthService } from '../../services/auth.service';
 import { ISnapshotData } from './volume-snapshot';
 
@@ -24,7 +25,7 @@ import { ISnapshotData } from './volume-snapshot';
     >
     </cs-volume-actions>`,
 })
-export class VolumeActionsContainerComponent extends WithUnsubscribe() {
+export class VolumeActionsContainerComponent implements AfterViewInit {
 
   @Input() public volume: Volume;
 
@@ -32,8 +33,11 @@ export class VolumeActionsContainerComponent extends WithUnsubscribe() {
     public dialogService: DialogService,
     public authService: AuthService,
     private store: Store<State>,
-  ) {
-    super();
+    private cd: ChangeDetectorRef
+  ) { }
+
+  public ngAfterViewInit() {
+    this.cd.detectChanges();
   }
 
   public onVolumeDelete(volume: Volume): void {
