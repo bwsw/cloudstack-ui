f88e908644e66885bc3bddfaf4780311b5c174ee
ksendart <ksendart@yandex.ru>
2017-12-04 15:02:56 +0700

    #670-sg-account-filter - add filter by account for security group page

--- src/app/security-group/sg-filter/sg-filter.component.ts
+++ src/app/security-group/sg-filter/sg-filter.component.ts
@@ -1,10 +1,16 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 import { SecurityGroupViewMode } from './containers/sg-filter.container';
 
 
 export interface SecurityGroupFilter {
   viewMode: SecurityGroupViewMode;
   query: string;
+  selectedAccountIds: Array<string>;
 }
 
 
@@ -14,17 +20,21 @@ export interface SecurityGroupFilter {
   styleUrls: ['sg-filter.component.scss']
 })
 export class SgFilterComponent {
+  @Input() public accounts: Array<Account>;
   @Output() public viewModeChange = new EventEmitter<SecurityGroupViewMode>();
   @Output() public queryChange = new EventEmitter<string>();
   @Output() public vmChange = new EventEmitter<string>();
+  @Output() public onAccountsChange = new EventEmitter<Array<string>>();
 
   public viewMode: SecurityGroupViewMode;
   public query: string;
+  public selectedAccountIds: Array<string>;
 
   @Input()
   public set filters(filter: SecurityGroupFilter) {
     this.viewMode = filter.viewMode;
     this.query = filter.query;
+    this.selectedAccountIds = filter.selectedAccountIds;
   }
 
   public get SecurityGroupViewMode() {
