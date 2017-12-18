08481250966853e382c2df44539ca288ee88525b
MaslovaEV <maslova_ev@bw-sw.com>
2017-12-18 16:37:24 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,13 +30,7 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.mathjax',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+extensions = ['sphinxjp.themes.basicstrap']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -88,14 +82,14 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'basicstrap'
 
 html_logo = 'cloudstack_logo_light.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {'sidebarbgcolor': '#6095c4', 'relbarbgcolor': '#6095c4'}
+# html_theme_options = {'sidebarbgcolor': '#6095c4', 'relbarbgcolor': '#6095c4'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
