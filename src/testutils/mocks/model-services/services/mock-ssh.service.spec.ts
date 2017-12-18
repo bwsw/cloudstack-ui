e7834e79916b13d253df8da441c3a61f040c90ce
Ivaschenko Christina <caloriees@gmail.com>
2017-12-18 12:28:42 +0700

    feat(ssh-key-model): Get rid of BaseModel for SSH Keys model - fixes

--- src/testutils/mocks/model-services/services/mock-ssh.service.spec.ts
+++ src/testutils/mocks/model-services/services/mock-ssh.service.spec.ts
@@ -3,11 +3,11 @@ import { Observable } from 'rxjs/Observable';
 import { SSHKeyPair } from '../../../../app/shared/models';
 
 
-const sshKeyPairs: Array<Object> = require('../fixtures/sshKeyPairs.json');
+const sshKeyPairs: Array<SSHKeyPair> = require('../fixtures/sshKeyPairs.json');
 
 @Injectable()
 export class MockSSHKeyPairService {
   public getList(): Observable<Array<SSHKeyPair>> {
-    return Observable.of(sshKeyPairs.map(json => json as SSHKeyPair));
+    return Observable.of(sshKeyPairs);
   }
 }
