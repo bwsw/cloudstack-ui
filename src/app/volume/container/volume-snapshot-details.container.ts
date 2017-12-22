bbe5e8ebd35517f7119cf532c8feaa443304396a
ksendart <ksendart@yandex.ru>
2017-12-22 11:29:13 +0700

    #742-volume-action - after review

--- src/app/volume/container/volume-snapshot-details.container.ts
+++ src/app/volume/container/volume-snapshot-details.container.ts
@@ -1,44 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { DialogService } from '../../dialog/dialog-service/dialog.service';
 import { State } from '../../reducers/index';
 import * as volumeActions from '../../reducers/volumes/redux/volumes.actions';
 import * as fromVolumes from '../../reducers/volumes/redux/volumes.reducers';
 import { Volume } from '../../shared/models/volume.model';
-import { WithUnsubscribe } from '../../utils/mixins/with-unsubscribe';
 
 
 @Component({
   selector: 'cs-volume-snapshot-details-container',
   template: `
     <cs-volume-snapshot-details
-      [volume]="volume"
+      [volume]="volume$ | async"
       (onSnapshotAdd)="addSnapshot($event)"
     >
     </cs-volume-snapshot-details>`,
 })
-export class VolumeSnapshotDetailsContainerComponent extends WithUnsubscribe() implements OnInit {
+export class VolumeSnapshotDetailsContainerComponent {
 
   readonly volume$ = this.store.select(fromVolumes.getSelectedVolume);
-
-  public volume: Volume;
 
   constructor(
     public dialogService: DialogService,
     private store: Store<State>,
   ) {
-    super();
   }
 
-  public ngOnInit() {
-    this.volume$
-      .takeUntil(this.unsubscribe$)
-      .subscribe(volume => {
-        if (volume) {
-          this.volume = new Volume(volume);
-        }
-      })
-  }
 
   public addSnapshot(volume: Volume): void {
     this.store.dispatch(new volumeActions.AddSnapshot(volume));
