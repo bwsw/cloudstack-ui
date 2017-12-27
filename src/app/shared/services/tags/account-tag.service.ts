15514fc122bed07e68bf853c513a82dd2bc3686f
Ivaschenko Christina <caloriees@gmail.com>
2017-12-27 14:09:00 +0700

    feat(ssh-description): Store "SSH description" in account tags if supported

--- src/app/shared/services/tags/account-tag.service.ts
+++ src/app/shared/services/tags/account-tag.service.ts
@@ -27,6 +27,7 @@ export class AccountTagService implements EntityTagService {
     return this.accountService.getAccount({name: this.user.account, domainid: this.user.domainid})
       .switchMap(account => {
           return this.tagService.getTag(account, this.getSshKeyDescriptionKey(sshKey))
+            .filter(tag => !!tag)
             .map(tag => this.tagService.getValueFromTag(tag));
         }
       );
