7e8e61e5fc4ef416f965013d74b5b654f6272c25
Darya Baklanova <darya.baklanova@gmail.com>
2018-01-11 16:25:59 +0700

    fix(vm-tags): fix vm tag creation bug

--- src/app/vm/vm-sidebar/vm-tags/vm-tags.component.ts
+++ src/app/vm/vm-sidebar/vm-tags/vm-tags.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnChanges, } from '@angular/core';
+import { VmResourceType } from '../../';
 import { DialogService } from '../../../dialog/dialog-service/dialog.service';
 import { TagService } from '../../../shared/services/tags/tag.service';
+import { KeyValuePair } from '../../../tags/tags-view/tags-view.component';
 import { TagsComponent } from '../../../tags/tags.component';
 import { VirtualMachine } from '../../shared/vm.model';
 
@@ -23,4 +25,20 @@ export class VmTagsComponent extends TagsComponent<VirtualMachine> implements On
     this.tags = this.entity && this.entity.tags;
   }
 
+  public addTag(tag: KeyValuePair): void {
+    if (!tag) {
+      return;
+    }
+
+    this.tagService.create({
+      resourceIds: this.entity.id,
+      resourceType: VmResourceType,
+      'tags[0].key': tag.key,
+      'tags[0].value': tag.value
+    })
+      .subscribe(
+        res => this.onTagAdd.emit(tag),
+        error => this.onError(error)
+      );
+  }
 }
