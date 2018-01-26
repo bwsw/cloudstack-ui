95a268a1cbfb7da04d192b9119917e1d53ccd89d
ksendart <ksendart@yandex.ru>
2018-01-26 12:12:09 +0700

    refactor(account-model):  Get rid of BaseModel for Account (#889)
    #777

--- src/app/shared/services/account.service.ts
+++ src/app/shared/services/account.service.ts
@@ -1,18 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { BackendResource } from '../decorators/backend-resource.decorator';
 import { Account } from '../models/account.model';
+import { AsyncJobService } from './async-job.service';
 import { BaseBackendService } from './base-backend.service';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 @BackendResource({
-  entity: 'Account',
-  entityModel: Account
+  entity: 'Account'
 })
 export class AccountService extends BaseBackendService<Account> {
 
-  constructor(protected http: HttpClient) {
+  constructor(
+    protected http: HttpClient,
+    private asyncJobService: AsyncJobService
+  ) {
     super(http);
   }
 
@@ -20,29 +23,31 @@ export class AccountService extends BaseBackendService<Account> {
     return this.getList(params).map(accounts => accounts[0]);
   }
 
-  public removeAccount(account: Account): Observable<{ jobid: string }> {
-    return this.sendCommand('delete', {
-      id: account.id,
-    });
+  public removeAccount(account: Account): Observable<Account> {
+    return this.sendCommand('delete', { id: account.id })
+      .switchMap(job => this.asyncJobService.queryJob(job))
+      .switchMap(response => Observable.of(account));
   }
 
-  public disableAccount(account: Account): Observable<{ jobid: string }> {
+  public disableAccount(account: Account): Observable<Account> {
     return this.sendCommand('disable', {
       id: account.id,
       lock: false
-    });
+    }).switchMap(job => this.asyncJobService.queryJob(job))
+      .switchMap(response => Observable.of(response.result.account));
   }
 
-  public lockAccount(account: Account): Observable<{ jobid: string }> {
+  public lockAccount(account: Account): Observable<Account> {
     return this.sendCommand('disable', {
       id: account.id,
       lock: true
-    });
+    }).switchMap(job => this.asyncJobService.queryJob(job))
+      .switchMap(response => Observable.of(response.result.account));
   }
 
-  public enableAccount(account: Account): Observable<{ account: Account }> {
+  public enableAccount(account: Account): Observable<Account> {
     return this.sendCommand('enable', {
       id: account.id
-    });
+    }).map(res => res.account);
   }
 }
