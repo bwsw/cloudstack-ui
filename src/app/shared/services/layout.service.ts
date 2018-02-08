1b66496625b71103ea412025c93f7f05914abcc5
Ivaschenko Christina <caloriees@gmail.com>
2018-02-08 14:31:03 +0700

    feat(sidebar): Keep the position of a left-side bar (collapsed/expanded)

--- src/app/shared/services/layout.service.ts
+++ src/app/shared/services/layout.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
+import { LocalStorageService } from './local-storage.service';
 
 @Injectable()
 export class LayoutService {
   public drawerOpen: boolean;
   public drawerToggled: Subject<void>;
 
-  constructor() {
-    this.drawerOpen = true;
+  constructor(
+    protected localStorageService: LocalStorageService
+  ) {
+    this.drawerOpen = this.localStorageService.read('sidebarDrawer') === 'true';
     this.drawerToggled = new Subject<void>();
   }
 
   public toggleDrawer(): void {
     this.drawerOpen = !this.drawerOpen;
+    this.localStorageService.write('sidebarDrawer', this.drawerOpen.toString());
   }
 }
