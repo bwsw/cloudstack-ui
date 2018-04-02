e0754a0a5d53411d92dce2a2524f4bcdad989eed
Daniel Tamazlykar <danieltamazlykar@gmail.com>
2018-04-02 18:52:29 +0700

    fixup! build(ci): update travis configuration to build and push docker images

--- scripts/publish-docker-image.sh
+++ scripts/publish-docker-image.sh
@@ -2,16 +2,9 @@
 
 # To run the docker login command non-interactively, you can set the --password-stdin flag to provide a password
 # through STDIN. Using STDIN prevents the password from ending up in the shell’s history, or log-files.
-
 echo $BWSW_DOCKER_PASSWORD | docker login --username $BWSW_DOCKER_USERNAME --password-stdin
 
-
-# Сreates a tagged image if the assembly was launched from the tagged comit
-
-if [ -z ${TRAVIS_TAG} ]
-then
-  docker push ${DOCKER_USER}/${DOCKER_REPO}
-else
-  docker tag ${DOCKER_USER}/${DOCKER_REPO} ${DOCKER_USER}/${DOCKER_REPO}:${TRAVIS_TAG}
-  docker push ${DOCKER_USER}/${DOCKER_REPO}:${TRAVIS_TAG}
-fi
+# Сreates a tagged image if the assembly was launched from the tagged commit
+TAG=${TRAVIS_TAG:-latest}
+docker tag ${DOCKER_USER}/${DOCKER_REPO} ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
+docker push ${DOCKER_USER}/${DOCKER_REPO}:${TAG}
