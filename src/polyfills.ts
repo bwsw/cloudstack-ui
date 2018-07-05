4ca4f3c01cdf259bd97fdee852b76865a4848e99
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-07-05 15:44:24 +0700

    add rxjs operator to global scope

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -64,16 +64,17 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/combineLatest';
 import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/onErrorResumeNext';
-import 'rxjs/add/operator/distinctUntilChanged';
-import 'rxjs/add/operator/finally';
-import 'rxjs/add/operator/pluck';
 import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/first'
+import 'rxjs/add/operator/pluck';
 import 'rxjs/add/operator/switchMapTo';
-import 'rxjs/add/operator/withLatestFrom';
+import 'rxjs/add/operator/takeUntil';
 import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/withLatestFrom';
 // static operator imports ( e.g. Observable.merge )
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/observable/throw';
