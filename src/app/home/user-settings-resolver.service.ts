e3fcd7b0a56dca8b49857039a8838ae6ced569f3
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-07-12 09:00:00 +0700

    router data resolver for loading user tags

---
+++ src/app/home/user-settings-resolver.service.ts
@@ -0,0 +1,21 @@
+/* Preload user settings before render main interface */
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+
+import { State, UserTagsActions, UserTagsSelectors } from '../root-store';
+
+@Injectable()
+export class UserSettingsResolver implements Resolve<boolean> {
+  constructor(private store: Store<State>) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    this.store.dispatch(new UserTagsActions.LoadUserTags());
+
+    return this.store.select(UserTagsSelectors.getIsLoading)
+      .filter(loading => loading === false)
+      .first();
+  }
+}
