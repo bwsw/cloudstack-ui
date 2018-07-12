d22ca9ba43d64e20b9efc197c4b45f5f9c16aa6e
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-07-12 08:48:19 +0700

    implement root-store with user-tags

---
+++ src/app/root-store/user-tags/user-tag-keys.ts
@@ -0,0 +1,13 @@
+export const userTagKeys = {
+  askToCreateVM: 'csui.user.ask-to-create-vm',
+  askToCreateVolume: 'csui.user.ask-to-create-volume',
+  savePasswordForAllVMs: 'csui.user.save-password-for-all-vms',
+  firstDayOfWeek: 'csui.user.first-day-of-week',
+  lang: 'csui.user.lang',
+  lastVMId: 'csui.user.last-vm-id',
+  sessionTimeout: 'csui.user.session-timeout',
+  showSystemTags: 'csui.user.show-system-tags',
+  timeFormat: 'csui.user.time-format',
+  theme: 'csui.user.theme',
+  navigationOrder: 'csui.user.navigation-order'
+};
