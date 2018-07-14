06078761ede1a1fa3bfdff22cba957ad194d8446
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-07-14 15:45:38 +0700

    use ngrx/entity for user-tags

--- src/app/root-store/user-tags/user-tags.reducer.ts
+++ src/app/root-store/user-tags/user-tags.reducer.ts
@@ -1,31 +1,8 @@
+import { Update } from '@ngrx/entity';
+
 import { UserTagsActionsUnion, UserTagsActionTypes } from './user-tags.actions';
+import { adapter, initialState, UserTagsState } from './user-tags.state';
 import { Tag } from '../../shared/models';
-import { initialState, UserTagsState } from './user-tags.state';
-
-
-function getEntities(entities: Tag[]): { [id: string]: Tag } {
-  return entities.reduce((dictionary, entity) => {
-    dictionary[entity.key] = entity;
-    return dictionary;
-  }, {})
-}
-
-function getIds(entities: Tag[]): string[] {
-  return entities.map(tag => tag.key)
-}
-
-function upsertMany(entities: Tag[], state: UserTagsState): UserTagsState {
-  const upsertIds = getIds(entities);
-  const upsertEntities = getEntities(entities);
-  return {
-    ...state,
-    ids: [...state.ids, ...upsertIds],
-    entities: {
-      ...state.entities,
-      ...upsertEntities
-    }
-  }
-}
 
 export function reducer(state = initialState, action: UserTagsActionsUnion): UserTagsState {
   switch (action.type) {
@@ -37,8 +14,8 @@ export function reducer(state = initialState, action: UserTagsActionsUnion): Use
     }
 
     case UserTagsActionTypes.USER_TAGS_LOADED: {
-      const tags: Tag[] = action.payload.tags;
-      return upsertMany(tags, { ...state, isLoading: false });
+      const updates: Update<Tag>[] = action.payload.tags.map(tag => ({ id: tag.key, changes: tag }));
+      return adapter.upsertMany(updates, { ...state, isLoading: false });
     }
 
     case UserTagsActionTypes.USER_TAGS_LOAD_ERROR: {
