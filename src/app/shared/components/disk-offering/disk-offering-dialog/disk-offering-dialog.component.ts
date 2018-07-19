4867ed39126a2e9626d0d2f1bbd07009a3f4f02c
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-07-19 15:42:19 +0700

    fixup! fix: do not disable a select button for the already selected value

--- src/app/shared/components/disk-offering/disk-offering-dialog/disk-offering-dialog.component.ts
+++ src/app/shared/components/disk-offering/disk-offering-dialog/disk-offering-dialog.component.ts
@@ -12,7 +12,6 @@ import * as moment from 'moment';
 export class DiskOfferingDialogComponent {
   public diskOfferings: MatTableDataSource<DiskOffering>;
   public selectedDiskOffering: DiskOffering;
-  public preselectedDiskOffering: DiskOffering;
   public columns: Array<string>;
   public columnsToDisplay: string[];
   public customFields = ['provisioningtype', 'storagetype', 'iscustomized'];
@@ -25,7 +24,6 @@ export class DiskOfferingDialogComponent {
   ) {
     this.diskOfferings = new MatTableDataSource<DiskOffering>(data.diskOfferings);
     this.selectedDiskOffering = data.diskOffering;
-    this.preselectedDiskOffering = data.diskOffering;
     this.columns = data.columns;
     this.columnsToDisplay = [...this.columns, 'radioButton'];
   }
