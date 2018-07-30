fcaf5cf95ac9aedf8292051c4d82e3ebf5bc4045
Ivaschenko Christina <ivaschenko_ks@bw-sw.com>
2018-07-30 14:46:19 +0700

    refactor(account-model): get rid of BaseModel for Account User

--- src/app/shared/models/account-user.model.ts
+++ src/app/shared/models/account-user.model.ts
@@ -1,29 +1,28 @@
 import { TimeZone } from '../components/time-zone/time-zone.service';
 import { AccountType } from './account.model';
-import { BaseModelInterface } from './base.model';
 
-export class AccountUser implements BaseModelInterface {
-  public id?: string;
-  public username?: string;
-  public firstname?: string;
-  public lastname?: string;
-  public email?: string;
-  public password?: string;
-  public created?: string;
-  public state?: string;
-  public account?: string;
-  public accounttype?: number;
-  public roleid?: string;
-  public roletype?: AccountType;
-  public rolename?: AccountType;
-  public domain?: string;
-  public domainid?: string;
-  public timezone?: string;
-  public accountid?: string;
-  public iscallerchilddomain?: boolean;
-  public isdefault?: boolean;
-  public secretkey?: string;
-  public apikey?: string;
+export class AccountUser {
+  id?: string;
+  username?: string;
+  firstname?: string;
+  lastname?: string;
+  email?: string;
+  password?: string;
+  created?: string;
+  state?: string;
+  account?: string;
+  accounttype?: number;
+  roleid?: string;
+  roletype?: AccountType;
+  rolename?: AccountType;
+  domain?: string;
+  domainid?: string;
+  timezone?: string;
+  accountid?: string;
+  iscallerchilddomain?: boolean;
+  isdefault?: boolean;
+  secretkey?: string;
+  apikey?: string;
 }
 
 export interface ApiKeys {
