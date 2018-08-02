d7c2952a4b12001cc1a72f946b46bc3a15836302
HeyRoach <ivaschenko_ks@bw-sw.com>
2018-08-02 15:09:44 +0700

    refactor(snapshot-policy): get rid of BaseModel for SnapshotPolicy

--- src/app/snapshot/recurring-snapshots/snapshot-policy.model.ts
+++ src/app/snapshot/recurring-snapshots/snapshot-policy.model.ts
@@ -1,25 +1,13 @@
-import { FieldMapper } from '../../shared/decorators';
-import { BaseModel } from '../../shared/models';
+import { BaseModelInterface } from '../../shared/models';
 import { PolicyType } from './snapshot-policy-type';
 
 
-@FieldMapper({
-  fordisplay: 'forDisplay',
-  intervaltype: 'intervalType',
-  maxsnaps: 'maxSnaps',
-  timezone: 'timeZone',
-  volumeid: 'volumeId'
-})
-export class SnapshotPolicy extends BaseModel {
-  public id: string;
-  public forDisplay: boolean;
-  public intervalType: PolicyType;
-  public maxSnaps: number;
-  public schedule: string;
-  public timeZone: string;
-  public volumeId: string;
-
-  constructor(json) {
-    super(json);
-  }
+export interface SnapshotPolicy extends BaseModelInterface {
+  id: string;
+  fordisplay: boolean;
+  intervaltype: PolicyType;
+  maxsnaps: number;
+  schedule: string;
+  timezone: string;
+  volumeid: string;
 }
