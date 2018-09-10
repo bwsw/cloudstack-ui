56d7ff0ebfdc8168ec7a31782a476947dea49044
Кристина Иващенко <caloriees@GMAIL.COM>
2018-09-10 13:50:46 +0700

    fix: add ssh vm tags and support them (#1266)
    PR Close #1226

---
+++ src/app/vm/services/access.service.ts
@@ -0,0 +1,24 @@
+import { VirtualMachine } from '../';
+import { Tag } from '../../shared/models';
+
+export enum AuthModeType {
+  SSH = 'ssh',
+  HTTP = 'http',
+  VNC = 'vnc'
+}
+
+export abstract class AccessService {
+  protected readonly abstract authMode: AuthModeType;
+
+  public openWindow(vm: VirtualMachine) {
+    const address = this.getAddress(vm);
+    window.open(address, this.authMode, 'resizable=0,width=820,height=640');
+  }
+
+  protected getTagValue(tags: Tag[], key: string): string | undefined {
+    const foundTag = tags.find(tag => tag.key === key);
+    return foundTag && foundTag.value
+  }
+
+  protected abstract getAddress(vm: VirtualMachine);
+}
