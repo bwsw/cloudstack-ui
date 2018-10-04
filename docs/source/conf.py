41159a47dd8f2f995b1baed100dc280a66d29f65
MaslovaEV <maslova_ev@bw-sw.com>
2018-10-04 13:38:28 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,13 +63,14 @@ release = u'1.410.18'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'en'
-locale_dirs = ['locale/']
-gettext_compact = True
+locale_dirs = ['locale/']   # path is example but recommended.
+gettext_uuid = True
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ['_build']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
