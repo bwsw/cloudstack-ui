f3c383541527e146d769b82ec8af11954c5c30a2
MaslovaEV <maslova_ev@bw-sw.com>
2018-10-04 14:15:20 +0700

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -70,7 +70,7 @@ gettext_compact = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build']
+exclude_patterns = []
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
