65ff76a833a3079c67732c9f2be275b3c82cd04d
wowshakhov <shakhov_vs@bw-sw.com>
2018-10-11 13:16:48 +0700

    fix action name

--- src/app/vm-logs/redux/vm-logs.actions.ts
+++ src/app/vm-logs/redux/vm-logs.actions.ts
@@ -1,8 +1,8 @@
 import { Action } from '@ngrx/store';
 import { VmLog } from '../models/vm-log.model';
 
-export const LOAD_VM_LOGS_REQUEST = '[VM Logs] LOAD_EVENTS_REQUEST';
-export const LOAD_VM_LOGS_RESPONSE = '[VM Logs] LOAD_EVENTS_RESPONSE';
+export const LOAD_VM_LOGS_REQUEST = '[VM Logs] LOAD_VM_LOGS_REQUEST';
+export const LOAD_VM_LOGS_RESPONSE = '[VM Logs] LOAD_VM_LOGS_RESPONSE';
 export const VM_LOGS_FILTER_UPDATE = '[VM Logs] VM_LOGS_FILTER_UPDATE';
 
 export class LoadVmLogsRequest implements Action {
