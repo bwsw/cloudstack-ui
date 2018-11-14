085031b74c18725f544bf8e4a75ec0bdc03e831c
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2018-11-14 09:11:14 +0700

    fixup! feat(sidebar): implement the ability to resize details sidebar

--- src/app/core/services/sidebar-width.service.ts
+++ src/app/core/services/sidebar-width.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { delay, filter, first, withLatestFrom } from 'rxjs/operators';
-import { State, UserTagsSelectors, layoutStore } from '../../root-store';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { layoutStore, State, UserTagsSelectors } from '../../root-store';
 
 /**
  * The service is used to get the size of the indent for floating elements relative to the sidebar,
@@ -24,20 +23,10 @@ export class SidebarWidthService {
   }
 
   private initializeSidebarWidthFromTag() {
-    this.store
-      .pipe(
-        select(UserTagsSelectors.getIsLoaded),
-        filter(Boolean),
-        delay(1),
-        first(),
-      )
-      .subscribe(() => {
-        this.store
-          .pipe(
-            select(layoutStore.selectors.getShowSidebar),
-            withLatestFrom(this.store.pipe(select(UserTagsSelectors.getSidebarWidth))),
-          )
-          .subscribe(([isOpen, width]: [boolean, number]) => this.setWidth(isOpen ? width : 0));
-      });
+    const width$ = this.store.pipe(select(UserTagsSelectors.getSidebarWidth));
+    const isSidebarOpen$ = this.store.pipe(select(layoutStore.selectors.getShowSidebar));
+    combineLatest(width$, isSidebarOpen$).subscribe(([width, isOpen]: [number, boolean]) =>
+      this.setWidth(isOpen ? width : 0),
+    );
   }
 }
