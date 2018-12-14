cc4de0ac03f16d68647e1c9ee083a7c80cacb5af
Daniil Tamazlykar <daniiltamazlykar@gmail.com>
2018-12-14 08:55:03 +0700

    feat(vm-snapshots): add ability to create, see list of vm snapshots in the vm sidebar (#1490)
    PR Close #928

--- src/app/root-store/server-data/vm-snapshots/vm-snapshots.selectors.ts
+++ src/app/root-store/server-data/vm-snapshots/vm-snapshots.selectors.ts
@@ -1,6 +1,7 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import * as vmSelectors from '../../../reducers/vm/redux/vm.reducers';
 import { vmSnapshotsFeatureName } from './vm-snapshots.reducer';
-import { VmSnapshotsState, adapter } from './vm-snapshots.state';
+import { adapter, VmSnapshotsState } from './vm-snapshots.state';
 
 const getVmSnapshotState = createFeatureSelector<VmSnapshotsState>(vmSnapshotsFeatureName);
 
@@ -9,3 +10,10 @@ export const { selectIds, selectEntities, selectAll, selectTotal } = adapter.get
 );
 
 export const getIsLoading = createSelector(getVmSnapshotState, state => state.isLoading);
+
+export const getVmSnapshotsForSelectedVm = createSelector(
+  selectAll,
+  vmSelectors.getSelectedId,
+  (vmSnapshots, selectedVmId) =>
+    vmSnapshots.filter(snapshot => snapshot.virtualmachineid === selectedVmId),
+);
--- src/app/root-store/server-data/vm-snapshots/vm-snapshots.state.ts
+++ src/app/root-store/server-data/vm-snapshots/vm-snapshots.state.ts
@@ -1,12 +1,19 @@
 import { createEntityAdapter, EntityAdapter, EntityState } from '@ngrx/entity';
 import { VmSnapshot } from '../../../shared/models/vm-snapshot.model';
+import moment = require('moment');
 
 export interface VmSnapshotsState extends EntityState<VmSnapshot> {
   isLoading: boolean;
 }
 
+const sortByCreationDate = (a: VmSnapshot, b: VmSnapshot) => {
+  // new snapshots come first
+  return moment(b.created).diff(moment(a.created));
+};
+
 export const adapter: EntityAdapter<VmSnapshot> = createEntityAdapter<VmSnapshot>({
   selectId: (vmSnapshot: VmSnapshot) => vmSnapshot.id,
+  sortComparer: sortByCreationDate,
 });
 
 export const initialState: VmSnapshotsState = adapter.getInitialState({
