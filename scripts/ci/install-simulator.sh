9291fa29bfb49ee19b1b8063f0bbe703caffdbe3
Daniil Tamazlykar <daniiltamazlykar@gmail.com>
2019-02-11 00:07:18 +0700

    change to ansibe container

--- scripts/ci/install-simulator.sh
+++ scripts/ci/install-simulator.sh
@@ -2,14 +2,17 @@
 
 set -u -o pipefail
 
-docker run --name cloudstack-simulator -d -p 4220:8888 tamazlykar/cs-sim-no-kafka
+SIMULATOR_IMAGE=quay.io/ansible/cloudstack-test-container
+STATUS_CHECK_PORT=8888
+
+docker run --name cloudstack-simulator -d -p 4220:8888 ${SIMULATOR_IMAGE}
 echo "Docker container is started"
 
 echo "Wait until simulator initialized"
 for i in $(seq 1 200); do
-  PORT_STATUS=$(curl -LI 127.0.0.1:4200 -o /dev/null -w '%{http_code}\n' -s);
+  PORT_STATUS=$(curl -LI 127.0.0.1:${STATUS_CHECK_PORT} -o /dev/null -w '%{http_code}\n' -s);
   echo -e "\n$PORT_STATUS";
-  docker logs --tail=20 cloudstack-simulator
+#  docker logs --tail=20 cloudstack-simulator  todo remove
   if [ "$PORT_STATUS" = "403" ]; then
     echo -e "\nSimulator initialization is done";
     break;
