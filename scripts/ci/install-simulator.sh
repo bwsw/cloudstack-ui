bb1e34eb8cbc376917981dc9fff12f257bfe0b7a
Daniil Tamazlykar <daniiltamazlykar@gmail.com>
2019-02-11 00:22:46 +0700

    change to my container

--- scripts/ci/install-simulator.sh
+++ scripts/ci/install-simulator.sh
@@ -2,7 +2,7 @@
 
 set -u -o pipefail
 
-SIMULATOR_IMAGE=quay.io/ansible/cloudstack-test-container
+SIMULATOR_IMAGE=tamazlykar/cs-sim-no-kafka
 STATUS_CHECK_PORT=8888
 
 docker run --name cloudstack-simulator -d -p 4220:8888 ${SIMULATOR_IMAGE}
@@ -11,12 +11,12 @@ echo "Docker container is started"
 echo "Wait until simulator initialized"
 for i in $(seq 1 200); do
   PORT_STATUS=$(curl -LI 127.0.0.1:${STATUS_CHECK_PORT} -o /dev/null -w '%{http_code}\n' -s);
-  echo -e "\n$PORT_STATUS";
 #  docker logs --tail=20 cloudstack-simulator  todo remove
   if [ "$PORT_STATUS" = "403" ]; then
     echo -e "\nSimulator initialization is done";
     break;
   fi;
   echo -en "\rChecking... ($i/200)";
+  echo -e "\n Server HTTP response $PORT_STATUS";
   sleep 5;
 done
