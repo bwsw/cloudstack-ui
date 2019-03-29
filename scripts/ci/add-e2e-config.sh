76df15f81f5efafec97c07439b80df27c1d72156
Daniil Tamazlykar <tamazlykar_dv@bw-sw.com>
2019-03-29 16:48:15 +0700

    test1

--- scripts/ci/add-e2e-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -u -e -o pipefail
-
-readonly thisDir=$(cd $(dirname $0); pwd)
-cp ${thisDir}/configs/e2e-config.json ${thisDir}/../../src/config/config.json
-
-echo -e "\nE2E application config added\n"
