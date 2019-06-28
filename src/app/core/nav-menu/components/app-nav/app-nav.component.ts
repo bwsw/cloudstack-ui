314b999bb6df7233546ec14c3d8bdecab9de0f52
huadream <admin@hjcns.com>
2019-06-28 13:23:14 +0800

    clean code

--- src/app/core/nav-menu/components/app-nav/app-nav.component.ts
+++ src/app/core/nav-menu/components/app-nav/app-nav.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 
 import { Route } from '../../models';
 
