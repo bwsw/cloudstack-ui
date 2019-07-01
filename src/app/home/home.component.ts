44ec8ddfbc54c99bc1c0d4e229eb10dc1f0c2c09
huadream <admin@hjcns.com>
2019-07-02 00:21:23 +0800

    fix visual problems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
-import { filter, first, take, takeUntil } from 'rxjs/operators';
+import { filter, takeUntil } from 'rxjs/operators';
 
-import { State, UserTagsActions, layoutStore } from '../root-store';
+import { State, UserTagsActions } from '../root-store';
 import { AuthService } from '../shared/services/auth.service';
 import { WithUnsubscribe } from '../utils/mixins/with-unsubscribe';
-import { Route, Subroute } from '../core/nav-menu/models';
-import { getCurrentRoute, getRoutes, getSubroutes } from '../core/nav-menu/redux/nav-menu.reducers';
+import { Route } from '../core/nav-menu/models';
+import { getCurrentRoute, getRoutes } from '../core/nav-menu/redux/nav-menu.reducers';
 import * as authActions from '../reducers/auth/redux/auth.actions';
 
 @Component({
@@ -18,10 +18,6 @@ import * as authActions from '../reducers/auth/redux/auth.actions';
 export class HomeComponent extends WithUnsubscribe() implements OnInit {
   public routes$: Observable<Route[]> = this.store.pipe(select(getRoutes));
   public currentRoute$: Observable<Route> = this.store.pipe(select(getCurrentRoute));
-  public subroutes$: Observable<Subroute[]> = this.store.pipe(select(getSubroutes));
-  public showAppNav$: Observable<boolean> = this.store.pipe(
-    select(layoutStore.selectors.getShowAppNav),
-  );
   public username: string;
 
   constructor(private auth: AuthService, private store: Store<State>) {
@@ -45,24 +41,5 @@ export class HomeComponent extends WithUnsubscribe() implements OnInit {
           }),
         );
       });
-  }
-
-  public openAppNav() {
-    this.store.dispatch(new layoutStore.actions.OpenAppNav());
-  }
-
-  public closeAppNav() {
-    this.store.dispatch(new layoutStore.actions.CloseAppNav());
-  }
-
-  // check and update state when it was closed not by button click,
-  // ex: onEsc or backdrop click
-  public closeAppNavInternal() {
-    this.showAppNav$
-      .pipe(
-        take(1),
-        filter(Boolean),
-      )
-      .subscribe(() => this.closeAppNav());
   }
 }
