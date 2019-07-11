08f07da5e3697504530e3102abd82c84c752cd86
huadream <admin@hjcns.com>
2019-07-11 16:49:01 +0800

    feature(navbar): new navbar

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,14 +5,27 @@ import { RouterModule } from '@angular/router';
 import { ConfigValidationService } from './config';
 import { SharedModule } from '../shared/shared.module';
 import { MaterialModule } from '../material/material.module';
-import { AppNavComponent, LicenseComponent, MenuHeaderComponent } from './nav-menu/components/';
+import {
+  AccountInfoComponent,
+  AppNavComponent,
+  LicenseComponent,
+  MenuHeaderComponent,
+} from './nav-menu/components/';
 import { SidebarWidthService, SnackBarService } from './services';
+import { NavbarComponent } from './navbar/components/navbar.component';
+import { NavbarService } from './services/navbar.service';
 
 @NgModule({
   imports: [CommonModule, SharedModule, MaterialModule, RouterModule],
-  exports: [AppNavComponent],
-  declarations: [AppNavComponent, LicenseComponent, MenuHeaderComponent],
-  providers: [ConfigValidationService, SnackBarService, SidebarWidthService],
+  exports: [AppNavComponent, NavbarComponent],
+  declarations: [
+    AppNavComponent,
+    LicenseComponent,
+    MenuHeaderComponent,
+    AccountInfoComponent,
+    NavbarComponent,
+  ],
+  providers: [ConfigValidationService, SnackBarService, SidebarWidthService, NavbarService],
 })
 export class CoreModule {
   constructor(
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
-
 import { State, UserTagsActions } from '../root-store';
 import { AuthService } from '../shared/services/auth.service';
 import { WithUnsubscribe } from '../utils/mixins/with-unsubscribe';
 import { Route } from '../core/nav-menu/models';
-import { getCurrentRoute, getRoutes } from '../core/nav-menu/redux/nav-menu.reducers';
+import {
+  getCurrentRoute,
+  getCurrentSubroute,
+  getRoutes,
+} from '../core/nav-menu/redux/nav-menu.reducers';
 import * as authActions from '../reducers/auth/redux/auth.actions';
+import { NavbarService } from '../core/services/navbar.service';
+import * as fromAccounts from '../reducers/accounts/redux/accounts.reducers';
+import { Account } from '../shared/models';
+const debounce = require('lodash/debounce');
 
 @Component({
   selector: 'cs-home',
@@ -18,11 +25,24 @@ import * as authActions from '../reducers/auth/redux/auth.actions';
 export class HomeComponent extends WithUnsubscribe() implements OnInit {
   public routes$: Observable<Route[]> = this.store.pipe(select(getRoutes));
   public currentRoute$: Observable<Route> = this.store.pipe(select(getCurrentRoute));
+  public currentSubroute$: Observable<Route> = this.store.pipe(select(getCurrentSubroute));
+  public account$: Observable<Account> = this.store.pipe(select(fromAccounts.selectUserAccount));
   public username: string;
+  public sideNavOpen = true;
+  public sideNavMode = window.innerWidth > 900 ? 'side' : 'over';
 
-  constructor(private auth: AuthService, private store: Store<State>) {
+  constructor(
+    private auth: AuthService,
+    private store: Store<State>,
+    private navbar: NavbarService,
+  ) {
     super();
-    this.username = this.auth.user ? this.auth.user.username : '';
+    this.username = this.auth.user ? `${this.auth.user.username} (${this.auth.user.domain})` : '';
+
+    // initial the navbar menu button's action
+    this.navbar.setDefaultButtonEvent(this.onMenuClick.bind(this));
+
+    this.onResize = debounce(this.onResize.bind(this), 100);
   }
 
   public ngOnInit(): void {
@@ -41,5 +61,31 @@ export class HomeComponent extends WithUnsubscribe() implements OnInit {
           }),
         );
       });
+
+    // When router changes and sideNavMode is 'over', hide the sideNav.
+    this.currentSubroute$.subscribe(() => {
+      if (this.sideNavMode === 'over' && this.sideNavOpen === true) {
+        this.sideNavOpen = false;
+      }
+    });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event) {
+    if (event.target.innerWidth > 900) {
+      if (this.sideNavMode !== 'side') {
+        this.sideNavOpen = true;
+      }
+      this.sideNavMode = 'side';
+    } else {
+      if (this.sideNavMode !== 'over') {
+        this.sideNavOpen = false;
+      }
+      this.sideNavMode = 'over';
+    }
+  }
+
+  public onMenuClick() {
+    this.sideNavOpen = !this.sideNavOpen;
   }
 }
