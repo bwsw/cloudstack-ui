813cbc54316301c6cd70a5da8212232837bd3527
Vladimir Shakhov <shakhov_vs@bw-sw.com>
2020-01-17 07:44:02 +0300

    Add comment to explain no interceptors

--- src/app/shared/services/http-client-no-interceptors.ts
+++ src/app/shared/services/http-client-no-interceptors.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpBackend } from '@angular/common/http';
 
+// to make requests without interceptors
+// e.g. to non-CS endpoints that don't need CS headers and params
 @Injectable()
 export class HttpClientNoInterceptors extends HttpClient {
   constructor(handler: HttpBackend) {
